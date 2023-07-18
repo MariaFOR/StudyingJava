e94e750bb1f6ddff46e94bd2619959c0a179c9ab
Fernanda Rabelo <fernandazg57@gmail.com>
2023-07-18 19:38:41 -0300

    first commit

---
+++ src/academy/devdojo/firstclasses/introduction/Classroom01HelloWorld.java
@@ -0,0 +1,19 @@
+package academy.devdojo.firstclasses.introduction;
+
+/**
+ * this is a <b>javadoc</b> comment
+ */
+
+// this is a one line comment
+
+    /*
+    this is a
+    multiline
+    comment
+     */
+
+public class Classroom01HelloWorld {
+    public static void main (String[] args){
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/academy/devdojo/firstclasses/introduction/Classroom02PrimitiveTypes.java
@@ -0,0 +1,9 @@
+package academy.devdojo.firstclasses.introduction;
+
+public class Classroom02PrimitiveTypes {
+    public static void main(String[] args){
+    //int, double, float, char, byte, short, long, boolean
+    int age = 10;
+    System.out.println("Age is " + age + " years");
+    }
+}
