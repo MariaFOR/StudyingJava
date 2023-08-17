d164970c4b783fcb9d0b94d57d6325723e39365a
Fernanda Rabelo <fernandazg57@gmail.com>
2023-08-17 18:50:16 -0300

    introduction to static modifier

---
+++ src/academy/devdojo/firstclasses/javacore/FstaticModifier/domain/Car.java
@@ -0,0 +1,36 @@
+package academy.devdojo.firstclasses.javacore.FstaticModifier.domain;
+
+public class Car {
+    private String name;
+    private double maximumSpeed;
+    public static double limitSpeed = 250;
+
+    public Car(String name, double maximumSpeed) {
+        this.name = name;
+        this.maximumSpeed = maximumSpeed;
+    }
+
+    public void printer(){
+        System.out.println("-----------------");
+        System.out.println("Name " + this.name);
+        System.out.println("Maximum Speed " + this.maximumSpeed);
+        System.out.println("Limit Speed " + Car.limitSpeed);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getMaximumSpeed() {
+        return maximumSpeed;
+    }
+
+    public void setMaximumSpeed(double maximumSpeed) {
+        this.maximumSpeed = maximumSpeed;
+    }
+
+}
---
+++ src/academy/devdojo/firstclasses/javacore/FstaticModifier/test/CarTest01.java
@@ -0,0 +1,17 @@
+package academy.devdojo.firstclasses.javacore.FstaticModifier.test;
+
+import academy.devdojo.firstclasses.javacore.FstaticModifier.domain.Car;
+
+public class CarTest01 {
+    public static void main(String[] args) {
+        Car c1 = new Car("BMW", 280);
+        Car c2 = new Car("Mercedes", 275);
+        Car c3 = new Car("Audi", 290);
+
+        Car.limitSpeed = 180;
+
+        c1.printer();
+        c2.printer();
+        c3.printer();
+    }
+}
